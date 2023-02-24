373d66c866d57756bf253598db37198ecc362222
Fuad Huseynov <fhuseynov803@gmail.com>
2023-02-24 12:05:50 +0400

    update

--- products/views.py
+++ products/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 from .models import Category, Product
 from django.db.models import Case, When, FloatField, F, Q, Count, Value, CharField
 from django.db.models.functions import Coalesce
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 
 def index_view(request):
-    filter_ = Q()
+    filter_, filter_dict = Q(), {}
     products = Product.objects.annotate(
             discount_percent=Coalesce(F("discount"), 0, output_field=FloatField())
         ).annotate(
@@ -29,6 +30,8 @@ def index_view(request):
         ).order_by("-created_at")
     category = request.GET.getlist("category", None)
     if category:
+        filter_dict["category"]="".join(f'category={cat}&' for cat in category)
+        print(filter_dict)
         filter_.add(Q(category__parent__parent_id__in=category), Q.AND)
         # products = products.filter(
         #     category__parent__parent_id__in=category
@@ -36,6 +39,7 @@ def index_view(request):
 
     price = request.GET.get("price", None)
     if price:
+        filter_dict["price"] = f"price={price}&"
         min_price, max_price = price.split(";")[0], price.split(";")[1]
         if min_price:
             filter_.add(Q(total_price__gte=float(min_price)), Q.AND)
@@ -46,6 +50,7 @@ def index_view(request):
 
     discount_list = request.GET.getlist("discount")
     if discount_list:
+        filter_dict["discount"] = "".join(f'discount={d}&' for d in discount_list)
         # products = products.annotate(
         #     discount_type=Case(
         #         When(discount__lt=5, then=Value('1')),
@@ -58,10 +63,17 @@ def index_view(request):
         # ).filter(discount_type__in=discount_list)
         filter_.add(Q(discount_type__in=discount_list), Q.AND)
 
-    print(filter_)
+
     products = products.filter(filter_)
+
+    paginator = Paginator(products, 1)
+    page = request.GET.get("page", 1)
+    product_list = paginator.get_page(page)
+
     context = {
         "categories": Category.objects.filter(parent__isnull=True).order_by("-created_at"),
-        "products": products
+        "products": product_list,
+        "paginator": paginator,
+        "filter_dict": filter_dict.values()
     }
     return render(request, "products/index.html", context)
